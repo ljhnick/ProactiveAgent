a95dd55288c1454e7886c4c7fba5991df20cce43
Nick Li <jiahao.nick@gmail.com>
2023-07-31 17:16:18 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages, distutils
+
+setup(name='proagent',
+      version='0.1',
+      description='',
+      author='ljhnick',
+      author_email='jiahao.nick@gmail.com',
+      license='Copyright',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=False)
---
+++ tests/test.py
@@ -0,0 +1,26 @@
+import torch
+from PIL import Image
+from lavis.models import load_model_and_preprocess
+
+
+def main():
+    device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
+    raw_image = Image.open("/home/nick/Research/ProactiveAgent/data/test/IMG_3635.jpg").convert("RGB")
+
+    model, vis_processors, _ = load_model_and_preprocess(
+        name='blip2_vicuna_instruct',
+        model_type='vicuna7b',
+        is_eval=True,
+        device=device,
+    )
+
+    image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
+
+    model.generate({"image": image, "prompt": "Write a short description for the image."})
+
+    print(1)
+    
+
+
+if __name__ == '__main__':
+    main()
